7b159574d1cbed59dcd4131eda5f97cb684d0040
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-08 19:57:57 +0200

    Create Comment model.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
@@ -12,5 +12,9 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 
+class CommentAdmin(adin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
 
-# Register your models here.
+admin.site.register(Comment, CommentAdmin)
