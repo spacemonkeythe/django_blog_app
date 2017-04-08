9575e79cb607f80446ddd88a4f13fd4e97733e89
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-08 17:56:17 +0200

    Implement share form - send blog post by email.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_list(request):
     object_list = Post.published.all()
@@ -23,4 +25,27 @@ def post_detail(request, year, month, day, post):
                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
-# Create your views here.
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recomends you reading "{}"'\
+                        .format(cleaned_data['name'], cleaned_data['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'\
+                        .format(post.title, post_url, cleaned_data['name'], cleaned_data['comments'])
+            send_mail(subject, message, 'test@test.com', [cleaned_data['to']])
+
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'test@test.com'
+EMAIL_HOST_PASSWORD = 'test'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
