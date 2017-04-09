93a3fe3c163ffd7517ead39feb90a848db45c1b4
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-09 13:46:28 +0200

    Implement -get similar posts- feature.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -27,12 +28,6 @@ def post_list(request, tag_slug=None):
     return render(request, 'blog/post/list.html', {'page': page,
                                                     'posts': posts,
                                                     'tag': tag})
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published',
-                            publish__year=year, publish__month=month,
-                            publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
@@ -65,6 +60,11 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                    .order_by('-same_tags', '-publish')[:4]
     comments = post.comments.filter(active=True)
 
     if request.method == 'POST':
@@ -83,4 +83,5 @@ def post_detail(request, year, month, day, post):
                     'blog/post/detail.html',
                     {'post': post,
                     'comments': comments,
-                    'comment_form': comment_form})
+                    'comment_form': comment_form,
+                    'similar_posts': similar_posts})
