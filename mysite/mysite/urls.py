e25ba88b8d615c40f7977ea3fe2e1e393abcb2b1
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-20 19:58:47 +0200

    Create sitemap from django.contrib.sites.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap
+}
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog'))
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
