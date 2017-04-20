4bd895cdaa303eefb8148b18a9f47b86c215acdf
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-04-20 20:14:32 +0200

    Add RSS feed with django.contrib.syndication.views.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from .feeds import LatestPostsFeed
 
 urlpatterns = [
     # post views
@@ -8,4 +9,5 @@ urlpatterns = [
         views.post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
